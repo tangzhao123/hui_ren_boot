75b8731b04a3854252ec8bd226383d4548425d7f
Administrator <11111111>
2021-07-21 17:00:29 +0800

    第十二次提交(药品xml修改 蔡)

---
+++ src/main/java/cn/gson/hui_ren_boot/controller/outpatient/RecipelController.java
@@ -0,0 +1,19 @@
+package cn.gson.hui_ren_boot.controller.outpatient;
+
+import cn.gson.hui_ren_boot.model.pojos.pharmacy.DrugInfo;
+import cn.gson.hui_ren_boot.model.service.outpatient.RecipelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * 门诊开处方
+ */
+@RestController
+@RequestMapping("/outpatient")
+public class RecipelController {
+
+}
