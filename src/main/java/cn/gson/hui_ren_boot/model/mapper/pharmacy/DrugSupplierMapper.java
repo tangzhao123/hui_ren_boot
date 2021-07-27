7695457b787bbf873b20402935696f5d7a3e3dbc
wangkaiya <2658236501@qq.com>
2021-07-27 11:10:44 +0800

    7月27 11（王文君）

--- src/main/java/cn/gson/hui_ren_boot/model/mapper/pharmacy/DrugSupplierMapper.java
+++ src/main/java/cn/gson/hui_ren_boot/model/mapper/pharmacy/DrugSupplierMapper.java
@@ -2,6 +2,7 @@ package cn.gson.hui_ren_boot.model.mapper.pharmacy;
 
 import cn.gson.hui_ren_boot.model.pojos.pharmacy.DrugSupplier;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -12,4 +13,13 @@ public interface DrugSupplierMapper {
 
     //查询供应商信息（药品弹框）
     public List<DrugSupplier> allSuppt(DrugSupplier drugSupplier);
+
+    //新增
+    public void addSupplier(DrugSupplier drugSupplier);
+
+    //修改
+    public void upSupplier(DrugSupplier drugSupplier);
+
+    //删除
+    public void deSupplier(@Param("supplierId") int drugSupplierId);
 }
