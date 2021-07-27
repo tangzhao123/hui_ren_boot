e1fb8ee8e1d8c4591611ec6967e94ea9476f5184
pangtianya <2475695516@qq.com>
2021-07-27 11:23:24 +0800

    7月27日（夏甜）

--- src/main/java/cn/gson/hui_ren_boot/model/mapper/nursestation/SickbedMapper.java
+++ src/main/java/cn/gson/hui_ren_boot/model/mapper/nursestation/SickbedMapper.java
@@ -1,5 +1,6 @@
 package cn.gson.hui_ren_boot.model.mapper.nursestation;
 
+import cn.gson.hui_ren_boot.model.pojos.hospital.Record;
 import cn.gson.hui_ren_boot.model.pojos.hospital.Register;
 import cn.gson.hui_ren_boot.model.pojos.nursestation.SickbedEntity;
 import org.apache.ibatis.annotations.Mapper;
@@ -8,6 +9,23 @@ import java.util.List;
 
 @Mapper
 public interface SickbedMapper{
+    //修改病床记录表
+    public void updateRecord(Record record);
+
+    //调换病床修改病床的状态
+    public void updateTwo(String sickbedMark);
+
+    //查询病床使用记录
+    public List<Record> selectRecord();
+
+    //修改病人资料，添加床位
+    public void updateRegister(Register register);
+
+    //新增病床使用记录
+    public void addRecord(Record record);
+
+    //修改病床状态
+    public void updateSickbed(String sickbedMark);
 
     //查询病床
     public List<SickbedEntity> bingchuang(Long medicalIdf);
