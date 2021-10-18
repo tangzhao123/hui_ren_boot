a0c5eb2ea76e1425dc7b98247cc12a6c0e840c16
tangzhao123 <1610468596@qq.com>
2021-10-18 19:54:36 +0800

    10月18号（何金钊）

--- src/main/java/cn/gson/hui_ren_boot/model/mapper/pharmacy/DruginfoMapper.java
+++ src/main/java/cn/gson/hui_ren_boot/model/mapper/pharmacy/DruginfoMapper.java
@@ -2,13 +2,14 @@ package cn.gson.hui_ren_boot.model.mapper.pharmacy;
 
 import cn.gson.hui_ren_boot.model.pojos.pharmacy.DrugInfo;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 @Mapper
 public interface DruginfoMapper {
     //查询处方选药
-    public List<DrugInfo> allDrug(@Param("typeName")String typeName,@Param("drugName")String drugName);
+    public List<DrugInfo> allDrug(@Param("typeName")String typeName, @Param("drugName")String drugName);
 
     //修改药品状态
     public void allState(DrugInfo drugInfo);
