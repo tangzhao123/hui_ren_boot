1b7fa015b878ecb121a495b2bb2983fb49adba8b
pangtianya <2475695516@qq.com>
2021-10-19 16:48:54 +0800

    10月19日（夏甜）第一次

--- src/main/java/cn/gson/hui_ren_boot/model/mapper/medical/TestMapper.java
+++ src/main/java/cn/gson/hui_ren_boot/model/mapper/medical/TestMapper.java
@@ -1,13 +1,28 @@
 package cn.gson.hui_ren_boot.model.mapper.medical;
 
+import cn.gson.hui_ren_boot.model.pojos.hospital.InspectDebit;
+import cn.gson.hui_ren_boot.model.pojos.medical.Combinspection;
 import cn.gson.hui_ren_boot.model.pojos.medical.Test;
 import cn.gson.hui_ren_boot.model.pojos.medical.Testmiddle;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 @Mapper
 public interface TestMapper {
+    //查看检验结果
+    public List<Combinspection> lookresult(@Param("inspectionPhone") String inspectionPhone);
+
+    //修改检验后的状态
+    public void payitem(String testAccount);
+
+    //修改预约的状态
+    public void updateDebit(String testPhone);
+
+    //预约缴费
+    public void addDebit(InspectDebit debit);
+
     //删除修改预约的状态
     public void updateYuyue(String testAccount);
 
